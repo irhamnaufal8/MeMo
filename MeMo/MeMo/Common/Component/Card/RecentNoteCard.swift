cd59556fd84a7b39f5f13e1db6ef3d970db49d3b
Irham Naufal <inaufal77@gmail.com>
2023-10-11 13:23:33 +0700

    feat(ButtonStyle): Create Scaled Button Style

---
+++ MeMo/MeMo/Common/Component/Card/RecentNoteCard.swift
@@ -0,0 +1,18 @@
+//
+//  RecentNoteCard.swift
+//  MeMo
+//
+//  Created by Irham Naufal on 11/10/23.
+//
+
+import SwiftUI
+
+struct RecentNoteCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentNoteCard()
+}
---
+++ MeMo/MeMo/Feature/Home/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MeMo
+//
+//  Created by Irham Naufal on 08/10/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
