b7285db79f60d4114009f8d8388c8e4e2de21c23
Irham Naufal <inaufal77@gmail.com>
2023-10-11 13:24:40 +0700

    feat(Component): Create folder card component

--- MeMo/MeMo/Common/Component/Card/FolderCard.swift
+++ MeMo/MeMo/Common/Component/Card/FolderCard.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct FolderCard: View {
+    
+    var image: String
+    var notes: Int
+    var title: String
+    var color: Color = .gray2
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            action()
+        } label: {
+            HStack(spacing: 8) {
+                Text(image)
+                    .font(.system(size: 36))
+                    .padding(8)
+                    .background(Color.white)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("\(notes) Notes")
+                        .font(.robotoBody)
+                        .foregroundColor(.black2)
+                    
+                    Text(title)
+                        .font(.robotoTitle3)
+                        .foregroundColor(.black1)
+                }
+                .lineLimit(1)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(color)
+            )
+        }
+        .scaledButtonStyle()
     }
 }
 
 #Preview {
-    FolderCard()
+    FolderCard(image: "💌", notes: 8, title: "Dummy Title") {}
 }
