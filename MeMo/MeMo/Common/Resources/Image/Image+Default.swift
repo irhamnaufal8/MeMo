2c4464f4214594b7b3b6adc424b6cebcdf9a9bad
Irham Naufal <inaufal77@gmail.com>
2023-10-17 22:16:18 +0700

    feat(Theme): Add theme color and app icon in app change logic

--- MeMo/MeMo/Common/Resources/Image/Image+Default.swift
+++ MeMo/MeMo/Common/Resources/Image/Image+Default.swift
@@ -9,4 +9,11 @@ import SwiftUI
 
 extension Image {
     static let dummy1 = Image("dummy1")
+    
+    static let logoRed = Image("logo-red")
+    static let logoOrange = Image("logo-orange")
+    static let logoGreen = Image("logo-green")
+    static let logoBlue = Image("logo-blue")
+    static let logoPurple = Image("logo-purple")
+    static let logoPink = Image("logo-pink")
 }
--- MeMo/MeMo/Feature/Home/ViewModel/HomeViewModel.swift
+++ MeMo/MeMo/Feature/Home/ViewModel/HomeViewModel.swift
@@ -13,6 +13,11 @@ final class HomeViewModel: ObservableObject {
     @Published var recentNotes: [NoteFile] = [.dummy, .dummy2, .dummy3, .dummy4]
     @Published var folders: [Folder] = [.dummy, .dummy2, .dummy3, .dummy4, .dummy5, .dummy6]
     
+    @Published var themes: [ThemeColor] = [.red, .orange, .green, .blue, .purple, .pink]
+    @AppStorage("current_theme") var currentTheme: String = ThemeColor.purple.rawValue
+    
+    @Published var isShowSheet = false
+    
     func countNotes(from folder: Folder) -> Int {
         folder.notes.count
     }
@@ -46,6 +51,68 @@ final class HomeViewModel: ObservableObject {
             return .red3
         default:
             return .gray2
+        }
+    }
+    
+    func secondaryColor(from color: String) -> Color {
+        switch color {
+        case ThemeColor.blue.rawValue:
+            return .blue2
+        case ThemeColor.green.rawValue:
+            return .green2
+        case ThemeColor.orange.rawValue:
+            return .orange2
+        case ThemeColor.pink.rawValue:
+            return .pink2
+        case ThemeColor.purple.rawValue:
+            return .purple2
+        case ThemeColor.red.rawValue:
+            return .red2
+        default:
+            return .gray2
+        }
+    }
+    
+    func accentColor(from color: String) -> Color {
+        switch color {
+        case ThemeColor.blue.rawValue:
+            return .blue1
+        case ThemeColor.green.rawValue:
+            return .green1
+        case ThemeColor.orange.rawValue:
+            return .orange1
+        case ThemeColor.pink.rawValue:
+            return .pink1
+        case ThemeColor.purple.rawValue:
+            return .purple1
+        case ThemeColor.red.rawValue:
+            return .red1
+        default:
+            return .gray2
+        }
+    }
+    
+    func appIcon(from color: String) -> Image {
+        switch color {
+        case ThemeColor.blue.rawValue:
+            return .logoBlue
+        case ThemeColor.green.rawValue:
+            return .logoGreen
+        case ThemeColor.orange.rawValue:
+            return .logoOrange
+        case ThemeColor.pink.rawValue:
+            return .logoPink
+        case ThemeColor.purple.rawValue:
+            return .logoPurple
+        default:
+            return .logoRed
+        }
+    }
+    
+    func selectTheme(_ theme: String) {
+        currentTheme = theme
+        UIApplication.shared.setAlternateIconName(theme) { _ in
+            self.isShowSheet = false
         }
     }
 }
