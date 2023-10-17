e3d59310d9b5fd3f8ea33e85a1b619e2d7cb4958
Irham Naufal <inaufal77@gmail.com>
2023-10-17 07:39:00 +0700

    feat(Note): Create note entity model

--- MeMo/MeMo/Common/Entities/Note.swift
+++ MeMo/MeMo/Common/Entities/Note.swift
@@ -5,4 +5,42 @@
 //  Created by Irham Naufal on 11/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct NoteFile: Identifiable {
+    var id: UUID = .init()
+    var title: String
+    var image: String
+    var notes: [any Note]
+    var theme: String
+}
+
+extension NoteFile {
+    static let dummy: NoteFile = .init(
+        title: "This is Dummy Title",
+        image: "🎮",
+        notes: [
+            NoteTextContent(text: "First sentence here.."),
+            NoteImageContent(image: .dummy1),
+            NoteTextContent(text: "\n"),
+            NoteTextContent(text: "Second sentence lalala. asldkjasd alskdja slkjjlakj alksjdalksjd laksjd alksj alkjlakjsla dlaks laksjd alskdj laksj ads"),
+            NoteImageContent(image: .dummy1)
+        ],
+        theme: ThemeColor.red.rawValue
+    )
+    
+    static let dummy2: NoteFile = .init(
+        title: "Shopping List",
+        image: "🍽️",
+        notes: [
+            NoteListContent(list: [
+                .init(isChecked: false, text: "Pencil"),
+                .init(isChecked: false, text: "Soap"),
+                .init(isChecked: false, text: "Shampoo"),
+                .init(isChecked: false, text: "Red T-Shirt"),
+                .init(isChecked: false, text: "Tomato"),
+            ])
+        ],
+        theme: ThemeColor.purple.rawValue
+    )
+}
---
+++ MeMo/MeMo/Common/Entities/ThemeColor.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeColor.swift
+//  MeMo
+//
+//  Created by Irham Naufal on 17/10/23.
+//
+
+import SwiftUI
+
+enum ThemeColor: String {
+    case blue = "BLUE"
+    case green = "GREEN"
+    case orange = "ORANGE"
+    case pink = "PINK"
+    case purple = "PURPLE"
+    case red = "RED"
+}
