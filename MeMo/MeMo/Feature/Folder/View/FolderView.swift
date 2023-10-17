5ddfa50fb5fde335075a37ec75196244b756d581
Irham Naufal <inaufal77@gmail.com>
2023-10-17 08:31:20 +0700

    feat(Component): Add date and color properties into recent note card

---
+++ MeMo/MeMo/Feature/Folder/View/FolderView.swift
@@ -0,0 +1,18 @@
+//
+//  FolderView.swift
+//  MeMo
+//
+//  Created by Irham Naufal on 17/10/23.
+//
+
+import SwiftUI
+
+struct FolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FolderView()
+}
