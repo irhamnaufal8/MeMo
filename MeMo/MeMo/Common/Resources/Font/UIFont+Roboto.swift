f6ec8140128a231e7287ef4768acd613f810ae67
Irham Naufal <inaufal77@gmail.com>
2023-10-17 07:33:59 +0700

    feat(Font): Add roboto font extension in UIFont

--- MeMo/MeMo/Common/Resources/Font/UIFont+Roboto.swift
+++ MeMo/MeMo/Common/Resources/Font/UIFont+Roboto.swift
@@ -5,4 +5,25 @@
 //  Created by Irham Naufal on 17/10/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    static let robotoTitle1 = UIFont(name: "Roboto-Bold", size: 24)
+    static let robotoTitle2 = UIFont(name: "Roboto-Bold", size: 21)
+    static let robotoTitle3 = UIFont(name: "Roboto-Bold", size: 18)
+    static let robotoHeadline = UIFont(name: "Roboto-Medium", size: 14)
+    static let robotoBody = UIFont(name: "Roboto-Regular", size: 14)
+    static let robotoCaption = UIFont(name: "Roboto-Regular", size: 12)
+    
+    static func robotoBold(size: CGFloat) -> UIFont? {
+        return .init(name: "Roboto-Bold", size: size)
+    }
+    
+    static func robotoMedium(size: CGFloat) -> UIFont? {
+        return .init(name: "Roboto-Medium", size: size)
+    }
+    
+    static func robotoRegular(size: CGFloat) -> UIFont? {
+        return .init(name: "Roboto-Regular", size: size)
+    }
+}
