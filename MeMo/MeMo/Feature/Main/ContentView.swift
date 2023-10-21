392d111789cc5af5b41ed738536141234124743c
Irham Naufal <inaufal77@gmail.com>
2023-10-22 03:15:54 +0700

    doc(Main): Add explanation comment in Main feature

--- MeMo/MeMo/Feature/Main/ContentView.swift
+++ MeMo/MeMo/Feature/Main/ContentView.swift
@@ -9,18 +9,31 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    
+
+    // An ObservableObject that manages the application's navigation state.
     @ObservedObject var navigator: AppNavigator = .init()
+
+    // A container for managing data models.
     let container: ModelContainer
 
+    // The body of the view.
     var body: some View {
+        // A NavigationStack that contains the HomeView and any other views that the application needs to navigate to.
+        // The NavigationStack's path property is bound to the navigator.routes property.
         NavigationStack(path: $navigator.routes) {
+            // The HomeView, which is the application's main view.
+            // The HomeView is injected with the ModelContainer and the AppNavigator.
             HomeView(modelContext: container.mainContext, navigator: navigator)
+                // A navigation destination for the HomeView.
+                // The navigation destination is specified using the Route enum.
                 .navigationDestination(for: Route.self) { $0 }
         }
+        // Sets the HomeViewModel for the environment.
+        // The HomeViewModel is injected with the ModelContainer.
         .environment(HomeView.HomeViewModel(modelContext: container.mainContext))
     }
 }
+
 //
 //#Preview {
 //    ContentView()
--- MeMo/MeMo/Feature/Main/MeMoApp.swift
+++ MeMo/MeMo/Feature/Main/MeMoApp.swift
@@ -5,38 +5,51 @@
 //  Created by Irham Naufal on 08/10/23.
 //
 
+// Import the SwiftUI and SwiftData frameworks.
 import SwiftUI
 import SwiftData
 
+// The main entry point for the application.
 @main
 struct MeMoApp: App {
-    
+
+    // A container for managing data models.
     let container: ModelContainer
-    
+
+    // Initializes the application.
     init() {
         do {
+            // Attempts to create a ModelContainer for the NoteFileResponse, NoteResponse, TagResponse, and FolderResponse models.
             container = try ModelContainer(for: NoteFileResponse.self, NoteResponse.self, TagResponse.self, FolderResponse.self)
         } catch {
+            // Fatally crashes the application if the ModelContainer could not be created.
             fatalError("Failed to create ModelContainer.")
         }
     }
-    
+
+    // The body of the application.
     var body: some Scene {
+        // Creates a WindowGroup that contains the ContentView.
         WindowGroup {
             ContentView(container: container)
         }
+        // Sets the ModelContainer for the WindowGroup.
         .modelContainer(container)
-        
     }
 }
 
+// An extension to the UINavigationController class that implements the UIGestureRecognizerDelegate protocol.
 extension UINavigationController: UIGestureRecognizerDelegate {
+
+    // Override the viewDidLoad() method to set the delegate of the interactivePopGestureRecognizer to self.
     override open func viewDidLoad() {
         super.viewDidLoad()
         interactivePopGestureRecognizer?.delegate = self
     }
 
+    // Implements the gestureRecognizerShouldBegin(_:) method to return true if the view controller stack contains more than one view controller.
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         return viewControllers.count > 1
     }
 }
+
